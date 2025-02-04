cce6126c1b30a1f86c7bc190815262ed1c33113d
YuraOstrik <134145610+YuraOstrik@users.noreply.github.com>
2025-02-04 15:20:03 +0200

    Add files via upload

---
+++ Basic class/Stationary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loading.Framework29._01.Basic_class
+{
+    internal class Stationery
+    {
+        public int Id {  get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public int Num { get; set; }
+        public decimal Price { get; set; }
+        public List<Sale> Sales { get; set; }
+    }
+}
